f4d89dfa62af3a71c342d625c19caf215468df9e
Romain Escudier <r.escudier@gmail.com>
2016-08-24 15:56:23 -0400

    RE: Add observations definition

--- Run_slurm/src/postprod_dart_obs.sh
+++ Run_slurm/src/postprod_dart_obs.sh
@@ -33,13 +33,15 @@ cat ${SCRATCHDIR}/input_${SIMU}.nml  | sed -e "s;<OBS_SEQ_LIST>;${LIST_FILES};g"
                                            -e "s;<DARTLOGNML>;${SCRATCHDIR}/Logs/DART/dart_post_c${disp_cycle}.out;g" \
 > ${SCRATCHDIR}/input.nml
 
+cd ${SCRATCHDIR}
+
 # Create diagnostic file
-./obs_diag
+${SCRATCHDIR}/Exe/obs_diag
 mv obs_diag_output.nc ${SCRATCHDIR}/Outputs/Diags/
 
 # Create netcdf observation files
-./obs_seq_to_netcdf
+${SCRATCHDIR}/Exe/obs_seq_to_netcdf
 mv obs_epoch_*.nc ${SCRATCHDIR}/Outputs/Diags/
-
+rename obs_epoch ${OBS_PREF} ${SCRATCHDIR}/Outputs/Diags/obs_epoch*.nc
 
 
