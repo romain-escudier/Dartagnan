a075bc1d506194d700c4e5dd6a15fbf4790fa769
Romain Escudier <r.escudier@gmail.com>
2016-09-08 16:56:17 -0400

    RE: New simulations!

---
+++ Run_slurm/user/romain/NWA-RE.REANA05/build_dart.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DARTDIR=/home/romain/Projects/DART/trunk/
+WORKDIR=/t6/workdir/romain/dartagnan/Run_slurm/user/romain/NWA-RE.REANA05/
+SCRATCHDIR=/t0/scratch/romain/dart/tmpdir_NWA-RE.REANA05/
+
+MYDIR=$(pwd)
+
+cd ${DARTDIR}/models/ROMS/work/
+
+./quickbuild.csh -mpi
+
+mkdir -p ${WORKDIR}/dart_exe/
+mkdir -p ${SCRATCHDIR}/Exe/
+cp roms_to_dart filter dart_to_roms obs_diag obs_seq_to_netcdf ${WORKDIR}/dart_exe/
+cp roms_to_dart filter dart_to_roms obs_diag obs_seq_to_netcdf ${SCRATCHDIR}/Exe/
+
+cd ${MYDIR}
+
