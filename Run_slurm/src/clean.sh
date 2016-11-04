ccc3a7c0780570a85c088cd9cd50c6d7d30d4ca7
Romain Escudier <r.escudier@gmail.com>
2016-11-04 16:52:54 -0400

    RE: Some updates on the code

--- Run_slurm/src/clean.sh
+++ Run_slurm/src/clean.sh
@@ -11,8 +11,10 @@
 echo "Clean the directory : ${SCRATCHDIR}"
 # Clean scratch directory
 rm -f ${SCRATCHDIR}/*
-rm -r ${SCRATCHDIR}/Outputs/
-rm -r ${SCRATCHDIR}/Roms_files
-rm -r ${SCRATCHDIR}/Tempfiles
-rm -r ${SCRATCHDIR}/Logs
+rm -r ${SCRATCHDIR}/Outputs/*
+rm -r ${SCRATCHDIR}/Roms_files/*
+rm -r ${SCRATCHDIR}/Tempfiles/*
+rm -f ${SCRATCHDIR}/Logs/*
+rm -f ${SCRATCHDIR}/Logs/DART/*
+rm -f ${SCRATCHDIR}/Logs/ROMS/*
 
